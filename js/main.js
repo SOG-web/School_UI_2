b660cb7baf56d5184d303505dbd4de9f921cff6e
SOG-web <raheemolalekanusman84@gmail.com>
2021-09-18 21:10:18 +0100

    m

--- js/main.js
+++ js/main.js
@@ -10,3 +10,21 @@ function myFunction() {
 $(function () {
   $('[data-toggle="popover"]').popover();
 });
+
+// var link = document.getElementById("db").getAttribute("href");
+// console.log(document.getElementById("db").href);
+// console.log(link);
+
+// if (screen.width < 650) {
+//   console.log(screen.width);
+// } else {
+// }
+
+// window.onload = function () {
+//   var link = document.getElementById("db").getAttribute("href");
+//   console.log(document.getElementById("db").getAttribute("href"));
+// };
+$(".db").on("click", function (e) {
+  console.log(e.target.href); // logs https://www.test.com/#test
+  console.log(e.target.hash); // logs #test
+});
